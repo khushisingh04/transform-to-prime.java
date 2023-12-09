d7f6c29d6295319e456edde51b2619c4bd077e40
khushisingh04 <145423064+khushisingh04@users.noreply.github.com>
2023-12-09 07:57:42 +0530

    Add files via upload

---
+++ Transform to prime.java
@@ -0,0 +1,28 @@
+//User function Template for Java
+
+
+class Solution
+{
+    public int minNumber(int arr[], int N)
+    {
+        int sum = 0;
+        for(int x: arr) sum+=x;
+        int ts = sum;
+        
+        if(sum==1) return 1;
+        while(true){
+            if(isPrime(sum)) break;
+            sum++;
+        }
+        return sum-ts;
+        
+    }
+    
+    private boolean isPrime(int n){
+        for(int i=2; i*i<=n; i++){
+            if(n%i==0) return false;
+        }
+        return true;
+    }
+}
+
